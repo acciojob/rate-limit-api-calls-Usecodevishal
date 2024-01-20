f006108aa5d0891293b1602afa8766fe8ad3be98
Vishal Bharti <100204338+Usecodevishal@users.noreply.github.com>
2024-01-21 01:08:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let count = 0;
+let coutn_el = document.querySelector("#results");
+coutn_el.innerText = `${count}`
+let btn_el = document.querySelector("#fetch-button");
+btn_el.addEventListener("click", function (e) {
+	//console.log("fetch data");
+	count++;
+	coutn_el.innerText = count;
+})
